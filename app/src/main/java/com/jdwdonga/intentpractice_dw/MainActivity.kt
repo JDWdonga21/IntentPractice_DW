0082565653dec8b825788f0cc19d2499d2f6e8a1
JDWdonga21 <ongame21212144@gmail.com>
2022-11-03 20:16:56 +0900

    화면 이동 기능 구현

--- app/src/main/java/com/jdwdonga/intentpractice_dw/MainActivity.kt
+++ app/src/main/java/com/jdwdonga/intentpractice_dw/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jdwdonga.intentpractice_dw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+//            다른 화면으로 이동
+//            Intent 활용
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
     }
 }
