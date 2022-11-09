0c7e7ec5d085008366d0b3cf24049fe6be154e74
jdwdonga21 <ongame21212144@gmail.com>
2022-11-09 17:17:55 +0900

    전달한 문구를 메시지 화면에서 활용

--- app/src/main/java/com/jdwdonga/intentpractice_dw/MessageActivity.kt
+++ app/src/main/java/com/jdwdonga/intentpractice_dw/MessageActivity.kt
@@ -1,11 +1,18 @@
 package com.jdwdonga.intentpractice_dw
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+//      이 화면에 들어올 때 첨부된 메시지 텍스트 뷰에 반영
+        val intentMessage = intent.getStringExtra("messages")
+
+        receivedMessage.text = intentMessage
     }
 }
