7b6825258393997ae8d15f125b395c8f201f2abb
jdwdonga21 <ongame21212144@gmail.com>
2022-11-09 18:06:35 +0900

    닉네임 변경화면 -> 입력 완료 클릭시 데이터 들고 돌아가기
    돌아간 메인화면에서 데이터 반영 작업 필요함

--- app/src/main/java/com/jdwdonga/intentpractice_dw/EditNickNameActivity.kt
+++ app/src/main/java/com/jdwdonga/intentpractice_dw/EditNickNameActivity.kt
@@ -1,11 +1,25 @@
 package com.jdwdonga.intentpractice_dw
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener {
+//            입력한 새 닉네임 저장
+            val inputNewNickname = newNicknameEdt.text.toString()
+//            입력한 닉네임을 가지고 메인으로 복귀
+//            입력한 닉네임을 담아주는 용도로 사용할 Intent
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNewNickname)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
